337665a583304d617765cc1e83fb8a3a3cdd9943
gasguzzler111 <sethan2003@gmail.com>
2025-05-30 21:23:15 -0400

    update

---
+++ Infernal-Descent/Assets/scrips/player_script.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class player_script : MonoBehaviour
+{
+    public float speed = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xspeed = Input.GetAxis("Horizontal");
+        float yspeed = Input.GetAxis("Vertical"); 
+        transform.Translate(Vector2.right * xspeed * speed * Time.deltaTime);
+        transform.Translate(Vector2.up * yspeed * speed * Time.deltaTime);  
+    }
+}
